dd2a9b01c4c8106fa39d9699c5f73624666a34fe
Pradnya Joshi <psjoshi20@gmail.com>
2021-02-26 17:42:27 +0800

    initial stage of fetching weather API

--- Document.md
+++ Document.md
@@ -1,6 +1,14 @@
-notes-
-create-react-app
-clear up project files
-project files structure
+notes-steps
+
+1)create-react-app
+2)clear up project files
+3) create project files structure
 
 create - React Functional Component, React Hooks, React Fetch API,
+4) get API data
+5) create first card component
+6)style
+7) city selector component
+8)usestate Hook
+9) Displaying API results
+10) to display our results we create another state
--- src/Component/TownSelector.js
+++ src/Component/TownSelector.js
@@ -1,28 +1,62 @@
 // second component Our goal is to update the state when a user clicks on a button
 // usestate - 
 import React, { useState } from 'react';
+import {Row,Col,FormControl,Button} from 'react-bootstrap';
 
 const TownSelector =()=> {
 const[town, setTown] = useState('');
+const [results, setResults] = useState('');
+
+const onSearch =() => {
+  fetch (
+    // fetch API ''
+    // dont use this -> this does not contain temp ' https://api.data.gov.sg/v1/environment/2-hour-weather-forecast'
+    'https://api.data.gov.sg/v1/environment/4-day-weather-forecast'
+  )
+  .then((response) => response.json())
+   .then(data => console.log(data));
+// update the results
+  //  .then((results) => setResults(results));
+};
+
 return(
     <div className="app-container">
       
       <div className="weather-traffic-wrapper"></div>
     
-      <div className="row">
+      <Row>
          <div class="col"> 
               <h1>Enter your Location </h1>
          </div>
-       </div>
-      <div className="row md-3">
+       </Row>
+       <Row>
+        {/* xs={4} takes the one third  of the page*/}
+        <Col xs={4} className="text-center">
+          <FormControl
+            placeholder="Enter Location"
+            // update town value with the user's input
+            onChange={(event) => setTown(event.target.value)}
+            // value will be the currently selected location or town
+            value={town}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+                <Button onClick={onSearch}> Check Weather </Button>
+        </Col>
+      </Row>
+
+
+      {/* <div className="row md-3">
           <input 
           type="text"
           name="town"
           className="form-control"
           placeholder="town"
           autoComplete="off" />
-      </div>
-     
+      </div> 
+      */}
       </div>
 )
 
