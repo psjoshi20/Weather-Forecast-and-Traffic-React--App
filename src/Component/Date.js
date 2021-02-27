339582e5780e2533d992496d2e060985a63d5ead
Pradnya Joshi <psjoshi20@gmail.com>
2021-02-27 11:43:02 +0800

    feature 1- fetch API -1

--- src/Component/Date.js
+++ src/Component/Date.js
@@ -22,3 +22,21 @@ class BootstrapDate extends React.Component{
 }
 
 export default BootstrapDate;
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// class App extends React.Component {
+//   state={
+//     curTime : new Date().toLocaleString(),
+//   }
+//   render(){
+//     return (
+//       <div className="App">
+//         <p>Current Time : {this.state.curTime}</p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
--- src/Component/LocationTraffic.js
+++ src/Component/LocationTraffic.js
@@ -1,26 +1,53 @@
 import React, { useState } from 'react';
-import {Row,Col,FormControl,Button} from 'react-bootstrap';
+import {Row,Col, Form , FormControl, Button  } from 'react-bootstrap';
+// import BootstrapDate from './Component/Date.js';
 
 const LocationTraffic =()=> {
 const[loc, setLoc] = useState('');
 const [results, setResults] = useState('');
 
-const onSearch =() => {
-  fetch (
+// const onSearch =() => {
+//   fetch (
    
-    'https://api.data.gov.sg/v1/transport/traffic-images'
-  )
-    .then((response) => response.json())
+//     'https://api.data.gov.sg/v1/transport/traffic-images'
+//   )
+//     .then((response) => response.json())
   
-    .then((result) => console.log(result));
-    // .then((results) => setResults(results));
+//     .then((result) => console.log(result));
+//     // .then((results) => setResults(results));
+// };
+const getTrafficData = async (query) => {
+  const res = await fetch(`'https://api.data.gov.sg/v1/transport/traffic-images'`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+  
+    },
+  });
+  const data = await res.json();
+  return data;
 };
 
-const onKeyDown = (event) => {
-  if (event.keyCode === 13) {
-    onSearch();
-  }
-};
+
+
+
+// const fetchTrafficImages = async () => {
+//   const res = await fetch(`https://api.data.gov.sg/v1/transport/traffic-images`, {
+//     method: "GET",
+//     headers: {
+//       "Content-Type": "application/json",
+     
+//     },
+//   });
+//   const data = res.json();
+//   return data;
+// };
+
+// const onKeyDown = (event) => {
+//   if (event.keyCode === 13) {
+//     onSearch();
+//   }
+// };
 
 return(
     <div className="app-container">
@@ -29,25 +56,38 @@ return(
     
       <Row>
          <div className="col"> 
-              <h1>Enter your Location </h1>
+              <h1>Choose your Date </h1>
+              {/* <BootstrapDate /> */}
+              <div>
+                <div className="row">
+                    <div className="col-md-4">
+                        <Form.Group controlId="dob">
+                            <Form.Label>Select Date</Form.Label>
+                            <Form.Control type="date" name="dob" placeholder="Date of Birth" />
+                        </Form.Group>
+                    </div>
+                </div>
+            </div>
+
          </div>
        </Row>
        <Row>
   
-        <Col xs={4} className="text-center">
+         {/* <Col xs={4} className="text-center">
           <FormControl
             placeholder="Enter Location"
 
             onChange={(event) => setLoc(event.target.value)}
 
-            value={loc}
+            value={loc} 
              onKeyDown={onKeyDown}
           />
-        </Col>
+        </Col> */}
       </Row>
       <Row>
         <Col>
-                <Button onClick={() => onSearch(loc)}> Get Traffic Image</Button>
+                {/* <Button onClick={() => fetchTrafficImages(loc)}> Get Traffic Image</Button> */}
+                <Button onClick={() => getTrafficData(loc)}> Get Traffic Image</Button>
         </Col>
       </Row>
 
